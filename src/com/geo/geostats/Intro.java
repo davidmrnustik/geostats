ec7e60e85dcded5506b6bc30b4e8a0dba8a6c48e
David Mrnustik <david.mrnustik@hotmail.com>
2013-06-11 07:31:45 +0200

    add new project

---
+++ src/com/geo/geostats/Intro.java
@@ -0,0 +1,15 @@
+package com.geo.geostats;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Intro extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.intro);
+	}
+    
+}
